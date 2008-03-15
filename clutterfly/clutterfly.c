2cd4180753f187f978f688d9bc0bf86a8cb59c0d
davidmaxwaterman <davidmaxwaterman@bab72b19-7648-0410-9316-dfcb6471b382>
2008-03-15 12:31:33 +0000

    checkpoint before moving into eclipse

--- clutterfly/clutterfly.c
+++ clutterfly/clutterfly.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <clutter/clutter.h>
 
+const guint noBoxes=4;
+
 static ClutterActor *
 clone_box (ClutterTexture *original, guint width, guint height, guint depth)
 {
@@ -49,10 +51,10 @@ clone_box (ClutterTexture *original, guint width, guint height, guint depth)
   /* move them to the correct distance from the origin */
   clutter_actor_set_depth (front, depth/2);
   clutter_actor_set_depth (back, -depth/2);
-  clutter_actor_set_position (left, width/2, 0);
+  clutter_actor_set_position (left,   width/2, 0);
   clutter_actor_set_position (right, -width/2, 0);
   clutter_actor_set_position (bottom, 0, -height/2);
-  clutter_actor_set_position (top, 0, height/2);
+  clutter_actor_set_position (top,    0,  height/2);
 
   clutter_actor_show_all (group);
   return group;
@@ -66,18 +68,21 @@ main (int argc, char *argv[])
   GError *error;
 
   /* dimensions */
-  guint stage_width, stage_height, stage_depth;
+  guint stage_width, stage_height, stage_depth, stage_width_far, stage_height_far;
   guint button_width, button_height, button_depth;
+  gfloat fovy, aspect, z_near, z_far;
 
   /* animation */
+  ClutterKnot path_begin[noBoxes], path_end;
   ClutterTimeline  *timeline;
-  ClutterBehaviour *t_behave;   /* translation from back stage to front stage */
-  ClutterBehaviour *r_behave_z; /* z component of tumble */
-  ClutterBehaviour *r_behave_y; /* y component of tumble */
+  ClutterBehaviour *t_behave_xy[noBoxes];   /* translation from xy to middle of screen */
+  ClutterBehaviour *t_behave_z;             /* translation from back stage to front stage */
+  ClutterBehaviour *r_behave_z; /* z axis component of tumble */
+  ClutterBehaviour *r_behave_y; /* y axis component of tumble */
 
   /* objects */
   ClutterActor     *stage;
-  ClutterActor     *group, *hand, *label, *rect, *janus, *box[4];
+  ClutterActor     *group, *hand, *label, *rect, *janus, *box[noBoxes];
   GdkPixbuf        *pixbuf;
 
   /* properties */
@@ -102,10 +107,17 @@ main (int argc, char *argv[])
   clutter_stage_set_fog (CLUTTER_STAGE (stage), 1.0, 10, -50);
 
   /* initialise dimensions */
+  clutter_stage_get_perspective (CLUTTER_STAGE(stage), &fovy, &aspect, &z_near, &z_far);
   clutter_actor_get_size (CLUTTER_ACTOR (stage), &stage_width, &stage_height);
-  button_width  = stage_width;
-  button_height = stage_height;
-  button_depth  = button_width/4;
+  stage_depth      = stage_width*4;
+  stage_width_far  = stage_width*z_far/z_near;
+  stage_height_far = stage_height*z_far/z_near;
+  button_width     = stage_width;
+  button_height    = stage_height;
+  button_depth     = button_width/4;
+  path_end.x       = stage_width/2;
+  path_end.y       = stage_height/2;
+
 
   /* signal to quit */
   g_signal_connect (stage,
@@ -119,15 +131,6 @@ main (int argc, char *argv[])
 
   /* make new timeline - 3 seconds, at 60 fps */
   timeline = clutter_timeline_new (180, 60);
-
-  for ( boxIndex=0; boxIndex<4; boxIndex++ )
-  {
-    /* create and orient button */
-    box[boxIndex] = clone_box (CLUTTER_TEXTURE (hand), button_width, button_height, button_depth);
-    clutter_container_add_actor (CLUTTER_CONTAINER (stage), box[boxIndex]);
-    clutter_actor_set_position (box[boxIndex], (boxIndex%2)*stage_width, (boxIndex/2)*stage_height);
-    clutter_actor_set_size (box[boxIndex], button_width, button_height);
-  }
 
   /* make behaviours */
   /* tumble */
@@ -150,20 +153,37 @@ main (int argc, char *argv[])
     0, 90);
 
   /* translate from back stage to front stage */
-  t_behave = clutter_behaviour_depth_new (
+  t_behave_z = clutter_behaviour_depth_new (
     clutter_alpha_new_full (
       timeline,
       CLUTTER_ALPHA_RAMP_INC,
       NULL, NULL),
-    -stage_width,
+    -stage_depth,
     0);
 
-  for ( boxIndex=0; boxIndex<4; boxIndex++ )
+  for ( boxIndex=0; boxIndex<noBoxes; boxIndex++ )
   {
+    path_begin[boxIndex].x = (boxIndex%2)*stage_width_far;
+    path_begin[boxIndex].y = (boxIndex/2)*stage_height_far;
+
+    /* create and orient button */
+    box[boxIndex] = clone_box (CLUTTER_TEXTURE (hand), button_width, button_height, button_depth);
+    clutter_container_add_actor (CLUTTER_CONTAINER (stage), box[boxIndex]);
+    clutter_actor_set_position (box[boxIndex], path_begin[boxIndex].x, path_begin[boxIndex].y);
+    clutter_actor_set_size (box[boxIndex], button_width, button_height);
+
+    /* translate from xy stage to middle of screen */
+    t_behave_xy[ boxIndex ] = clutter_behaviour_path_new (
+      clutter_alpha_new_full ( timeline, CLUTTER_ALPHA_RAMP_INC, NULL, NULL),
+      NULL, 0 );
+    clutter_behaviour_path_append_knot ( CLUTTER_BEHAVIOUR_PATH (t_behave_xy[ boxIndex ]), &path_begin[ boxIndex ] );
+    clutter_behaviour_path_append_knot ( CLUTTER_BEHAVIOUR_PATH (t_behave_xy[ boxIndex ]), &path_end );
+
     /* apply behaviours */
     clutter_behaviour_apply (r_behave_z, box[boxIndex]);
     clutter_behaviour_apply (r_behave_y, box[boxIndex]);
-    clutter_behaviour_apply (t_behave, box[boxIndex]);
+    clutter_behaviour_apply (t_behave_z, box[boxIndex]);
+    clutter_behaviour_apply (t_behave_xy[boxIndex], box[boxIndex]);
   }
 
   clutter_actor_show (stage);
@@ -174,7 +194,9 @@ main (int argc, char *argv[])
 
   g_object_unref (r_behave_z);
   g_object_unref (r_behave_y);
-  g_object_unref (t_behave);
+  g_object_unref (t_behave_z);
+  for ( boxIndex=0; boxIndex<noBoxes; boxIndex++ )
+	  g_object_unref (t_behave_xy[ boxIndex ]);
   g_object_unref (timeline);
 
   return EXIT_SUCCESS;
